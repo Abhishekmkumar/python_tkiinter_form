94776051e39f03b5c3010db99e53baf0e0fe8da4
Abhishek Manojkumar <159231714+Abhishekmkumar@users.noreply.github.com>
2024-08-17 15:01:32 +0530

    Add files via upload
    This Oracle SQL file is designed to create the form_details table, which is used in conjunction with the Python Tkinter form program. The table is structured to store user-submitted information such as first name, last name, gender, and email address.

---
+++ abhi hr.sql
@@ -0,0 +1,13 @@
+CREATE TABLE form_details
+(
+First_name VARCHAR2(30),
+Last_name VARCHAR2(30),
+Gender VARCHAR2(20),
+Email VARCHAR2(50)
+);
+
+desc FORM_DETAILS;
+
+SELECT * FROM form_details;
+
+TRUNCATE TABLE form_Details;
